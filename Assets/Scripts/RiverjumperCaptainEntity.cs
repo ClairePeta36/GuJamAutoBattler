4d2ad831f265e4061f15187712da27641db8b259
Claire Taylor-Cuthbertson <claire.taylorcuthbertson@immutable.com>
2022-12-22 14:03:28 +1000

    create RiverjumperCaptainEntity

---
+++ Assets/Scripts/RiverjumperCaptainEntity.cs
@@ -0,0 +1,56 @@
+﻿
+//At the start of combat, gain +(2) attack for each friendly Amazon squad. 
+
+public class RiverjumperCaptainEntity : BaseEntity
+{
+    protected override void OnRoundStart()
+    {
+        FindTarget();
+        foreach (var entity in GameManager.Instance.GetEntitiesAgainst(Team.Team2))
+        {
+            if (entity.getTribe() == Tribe.Amazon)
+            {
+                this.baseDamage += 2;
+            }
+        }
+    }
+
+    void FixedUpdate()
+    {
+        //this is for testing will be overridden in the new classes we create
+        if (!GameManager.Instance.GetIsGameRunning())
+        {
+            return;
+        }
+        // first we check if we have an active enemy target if not we find one
+        if (!HasEnemy)
+        {
+            FindTarget();
+        }
+        
+        // we then want to check is this enemy target we are aiming for in range to start attacking
+        if (IsEnemyInRange && !moving)
+        {
+            if (!canAttack)
+            {
+                return;
+            }
+            
+            //attack and deal damage
+            Attack();
+        }
+        else
+        {
+            // no enemy in range we need to keep moving
+            GetInRange();
+        }
+    }
+        
+    protected override void Attack()
+    {
+        base.Attack();
+
+        currentTarget.DealDamage(baseDamage);
+    }
+}
+
